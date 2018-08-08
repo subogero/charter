34b526fb53be3ca9a25a5b788fd815ba86b46bb5
SZABO Gergely <szg@subogero.com>
2018-08-08 13:27:49 -0400

    Copy Chart Permalink
    On a successful chart load, generate permalink with
    path/schema/select fields in the query string.
    Add button Copy Chart Permalink to copy it to clipboard.
    Load chart on page load if query string present.

--- public/charter.js
+++ public/charter.js
@@ -4,24 +4,24 @@ Object.filter = (o, f) => Object.assign({},
     .filter(f)
     .map(([k,v]) => ({[k]:v}))
 );
-const $ = {
+const _ = {
     id: id => document.getElementById(id),
     tag: tag => document.createElement(tag),
     txt: txt => document.createTextNode(txt),
-    appTxt: (el,txt) => { el.appendChild($.txt(txt)) },
+    appTxt: (el,txt) => { el.appendChild(_.txt(txt)) },
     clr: el => { while (1) { const c = el.firstChild; if (!c) { break } el.removeChild(c) } return el },
-    idClr: id => $.clr($.id(id)),
+    idClr: id => _.clr(_.id(id)),
     date2n: d => moment(d) / (86400000 * 365.2425) + 1970,
     n2date: n => moment((n - 1970) * 86400000 * 365.2425 + 1).format('YYYYMMDD'),
     y2date: y => y == null ? null : moment(y.replace(/^(....)(..)(..)$/, "$1-$2-$3")),
     lpw: 10,
-    leftpad: s => { s = s.substr(0,$.lpw); const ls = s.length; return ' '.repeat($.lpw-ls) + s },
+    leftpad: s => { s = s.substr(0,_.lpw); const ls = s.length; return ' '.repeat(_.lpw-ls) + s },
     resClasses: {},
     resClass: (el, ...args) => {
-        for (let key of Object.keys($.resClasses)) { el.classList.remove(key) }
+        for (let key of Object.keys(_.resClasses)) { el.classList.remove(key) }
         if (args.length == 0) { return }
         const key = args[0];
-        $.resClasses[key] = 1;
+        _.resClasses[key] = 1;
         el.classList.add(key);
     },
     access: (o, path, op) => {
@@ -35,8 +35,8 @@ const $ = {
         o[paths[max]] = op(o[paths[max]]);
         return o[paths[max]];
     },
-    get: (o, path) => $.access(o, path, x => x),
-    set: (o, path, val) => $.access(o, path, x => val),
+    get: (o, path) => _.access(o, path, x => x),
+    set: (o, path, val) => _.access(o, path, x => val),
     md5: md5_min,
     txt2html: txt =>
         txt == undefined ? '' :
@@ -47,6 +47,19 @@ const $ = {
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;")
         .replace(/\n/g, "<br>"),
+    query2obj: () => {
+        const query = window.location.search.substring(1);
+        if (!query)
+            return {};
+        const params = query.split('&');
+        return Object.assign({},
+           ...
+           params.map(p => {
+               const [k,v] = p.split('=');
+               return {[k]:decodeURIComponent(v)};
+           }),
+        );
+    },
 };
 
 function http(route, method, data) {
@@ -60,10 +73,10 @@ function http(route, method, data) {
             try { Object.assign(resp, JSON.parse(r.responseText)) }
             catch(err) {}
             if (r.status >= 200 && r.status < 300) {
-                $.id('err').innerHTML = '';
+                _.id('err').innerHTML = '';
                 resolve(resp);
             } else {
-                $.id('err').innerHTML = $.txt2html(resp.error);
+                _.id('err').innerHTML = _.txt2html(resp.error);
                 reject(Object.assign(resp, { status: r.status, statusText: r.statusText }));
             }
         };
@@ -76,7 +89,7 @@ function http(route, method, data) {
 }
 
 // Chart on canvas
-const canvas = $.id('chart');
+const canvas = _.id('chart');
 const ctx = canvas.getContext('2d');
 const yAxis = { id: 'Y', type: 'linear', position: 'left' };
 const tAxis = { id: 'T', type: 'linear', position: 'right', gridLines: { borderDash: [5,5] } };
@@ -120,9 +133,9 @@ const chart = new Chart(ctx, {
                     const key = data.datasets[tooltipItem.datasetIndex].label;
                     let val = tooltipItem.yLabel;
                     if (key.match(/_date$/)) {
-                        val = $.n2date(val);
+                        val = _.n2date(val);
                     }
-                    return $.leftpad(key) + '  ' + val;
+                    return _.leftpad(key) + '  ' + val;
                 },
                 footer: (a,d) => "Zoom: wheel drag double-click",
             },
@@ -145,14 +158,14 @@ canvas.ondblclick = ev => {
 };
 
 function updateChart(data) {
-    $.lpw = 0;
+    _.lpw = 0;
     chart.options.scales.xAxes[0].scaleLabel.labelString = data.x_label;
     let dateFound = false;
     let yFound = false;
     for (const dataset of data.datasets) {
         const lpw = dataset.label.length;
-        if ($.lpw < lpw) {
-            $.lpw = lpw;
+        if (_.lpw < lpw) {
+            _.lpw = lpw;
         }
         dataset.yAxisID = 'Y';
         if (! dataset.label.match(/_date$/i)) {
@@ -162,7 +175,7 @@ function updateChart(data) {
         dateFound = true;
         dataset.yAxisID = 'T';
         for (j = 0; j < dataset.data.length; j++) {
-            const d = $.date2n($.y2date(dataset.data[j]));
+            const d = _.date2n(_.y2date(dataset.data[j]));
             dataset.data[j] = d;
         }
     }
@@ -173,30 +186,30 @@ function updateChart(data) {
     chart.options.elements.line.borderWidth = 1;
     chart.data = data;
     chart.update();
-    console.log($.lpw);
+    console.log(_.lpw);
 }
 
 function tabExpand(el, el_hits, route, on_enter) {
-    $.id(el).onkeydown = function(ev) {
+    _.id(el).onkeydown = function(ev) {
         const self = this;
-        $.resClass(self);
+        _.resClass(self);
         const key = ev.keyCode;
-        const hits = $.id(el_hits);
-        $.clr(hits);
+        const hits = _.id(el_hits);
+        _.clr(hits);
         if (key != 9 && key != 13) { return }
         const txt = self.value;
         console.log("tabExpand", txt);
         ev.preventDefault();
         const method = key == 9 ? "GET" : "POST";
-        $.resClass(self, 'processing');
+        _.resClass(self, 'processing');
         http(route + txt, method)
         .then(resp => {
             if (key == 9) { // TAB
                 self.value = resp.path;
                 for (const respHit of resp.hits) {
                     if (resp.hits.length == 1) break;
-                    const hit = $.tag('div');
-                    $.appTxt(hit, respHit);
+                    const hit = _.tag('div');
+                    _.appTxt(hit, respHit);
                     hits.appendChild(hit);
                 }
             } else if (key == 13) { // ENTER
@@ -204,22 +217,22 @@ function tabExpand(el, el_hits, route, on_enter) {
             }
         })
         .catch(resp => {
-            $.resClass(self, 'failed');
+            _.resClass(self, 'failed');
         });
     }
 }
 
 function storeQuery() {
-    const schema = $.id("schema").value.split('/')[1];
-    const query = $.id("select").value;
-    const md5 = $.md5(query);
+    const schema = _.id("schema").value.split('/')[1];
+    const query = _.id("select").value;
+    const md5 = _.md5(query);
     const key = 'query-' + schema + '-' + md5;
     localStorage[key] = query;
 }
 
 function getQueries() {
-    const schema = $.id("schema").value.split('/')[1];
-    const query = $.id("select").value;
+    const schema = _.id("schema").value.split('/')[1];
+    const query = _.id("select").value;
     const re_query = '^' + query.replace(/[.^$+*?{}()\[\]]/g, "\\$&");
     return Object.values(Object.filter(
         localStorage,
@@ -227,39 +240,66 @@ function getQueries() {
     ));
 }
 
-tabExpand("path", "hits", "/home/", resp => {
+// path input field
+function path_load(resp) {
+    _.id('plink').value =
+        window.location.origin +
+        "?path=" + encodeURI(_.id("path").value);
     updateChart(resp.data)
-    $.resClass($.id('path'), 'loaded');
-    $.resClass($.id('select'));
-});
-$.id("path").onkeyup = ev => {
-    if (ev.keyCode == 40) { $.id("schema").focus() }
+    _.resClass(_.id('path'), 'loaded');
+    _.resClass(_.id('select'));
+}
+tabExpand("path", "hits", "/home/", path_load);
+_.id("path").onkeyup = ev => {
+    if (ev.keyCode == 40) { _.id("schema").focus() }
 };
 
+// schema input field
 let dbh = '';
-tabExpand("schema", "dbhits", "/db/", resp => {
+function schema_load(resp) {
     dbh = resp.dbh
-    $.resClass($.id('schema'), 'connected');
-    $.id('select').focus();
-});
-$.id("schema").onkeyup = ev => {
-    if (ev.keyCode == 38) { $.id("path").focus() }
+    _.resClass(_.id('schema'), 'connected');
+    _.id('select').focus();
+}
+tabExpand("schema", "dbhits", "/db/", schema_load);
+_.id("schema").onkeyup = ev => {
+    if (ev.keyCode == 38) { _.id("path").focus() }
 };
 
-$.id('select').onkeydown = function(ev) {
+// select input field
+function send_select(self) {
+    _.resClass(self, 'processing');
+    _.idClr('plink');
+    http('/select', 'POST', { schema: dbh, query: self.value })
+    .then(resp => {
+        _.id('plink').value =
+            window.location.origin +
+            "?schema=" + encodeURIComponent(_.id("schema").value) +
+            "&select=" + encodeURIComponent(_.id("select").value);
+        updateChart(resp.data);
+        _.resClass(self, 'loaded');
+        _.resClass(_.id('path'));
+        storeQuery();
+    })
+    .catch(err => {
+        _.id('err').innerHTML = err;
+        _.resClass(self, 'failed');
+    });
+}
+_.id('select').onkeydown = function(ev) {
     const self = this;
-    $.resClass(self);
-    const hits = $.id('qhits');
-    $.clr(hits);
+    _.resClass(self);
+    const hits = _.id('qhits');
+    _.clr(hits);
     if (ev.keyCode == 9) {
         ev.preventDefault();
         for (const query of getQueries()) {
-            const hit = $.tag('div');
-            $.appTxt(hit, query);
+            const hit = _.tag('div');
+            _.appTxt(hit, query);
             hit.onclick = () => {
                 self.value = query;
                 self.focus();
-                $.clr(hits);
+                _.clr(hits);
             };
             hits.appendChild(hit);
         }
@@ -267,26 +307,44 @@ $.id('select').onkeydown = function(ev) {
     } else if (!ev.ctrlKey || ev.keyCode != 13) {
         return;
     }
-    $.resClass(self, 'processing');
-    http('/select', 'POST', { schema: dbh, query: self.value })
-    .then(resp => {
-        updateChart(resp.data);
-        $.resClass(self, 'loaded');
-        $.resClass($.id('path'));
-        storeQuery();
-    })
-    .catch(resp => {
-        $.resClass(self, 'failed');
-    });
+    send_select(self);
 };
-$.id("select").onkeyup = ev => {
-    if (ev.keyCode == 27) { $.id("schema").focus() }
+_.id("select").onkeyup = ev => {
+    if (ev.keyCode == 27) { _.id("schema").focus() }
 };
 
 // Help
-$.id('ahelp').onclick = ev => { $.id('help').style.display = 'block' };
-$.id('help').onclick = ev => { ev.currentTarget.style.display = 'none' };
+_.id('ahelp').onclick = ev => { _.id('help').style.display = 'block' };
+_.id('help').onclick = ev => { ev.currentTarget.style.display = 'none' };
 document.onkeypress = ev => {
-    if (ev.key == 'F1')     { $.id('help').style.display = 'block' }
-    if (ev.key == 'Escape') { $.id('help').style.display = 'none' }
+    if (ev.key == 'F1')     { _.id('help').style.display = 'block' }
+    if (ev.key == 'Escape') { _.id('help').style.display = 'none' }
+};
+
+// Permalink support
+window.onload = ev => {
+    const query = _.query2obj();
+    console.log(query);
+    for (const [key,val] of Object.entries(query)) {
+        const input = _.id(key);
+        if (input == null)
+            continue;
+        input.value = val;
+        if (key == 'path') {
+            http('/home/' + val, 'POST')
+            .then(path_load)
+            .catch(err => { _.id('err').innerHTML = err; _.resClass(_.id('path'), 'failed') });
+        } else if (key == 'schema') {
+            dbh = val.split('/')[1];
+            http('/db/' + val, 'POST')
+            .then(schema_load)
+            .catch(err => { _.id('err').innerHTML = err; _.resClass(_.id('schema'), 'failed') });
+        } else if (key == 'select') {
+            send_select(_.id('select'));
+        }
+    }
+    _.id('clink').onclick = function(ev) {
+        _.id('plink').select();
+        document.execCommand('copy');
+    }
 };
